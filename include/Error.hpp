2390dc12d885dc42eda90bdb4ec0335c5a1f92ee
saberman888 <saberman8881@gmail.com>
2017-10-28 00:32:46 -0600

    json_error function added and edited some of the other functions to have DEBUG tags

--- include/Error.hpp
+++ include/Error.hpp
@@ -18,4 +18,5 @@ void unknown_error( Status& s );
 void bad_alloc_error( Status& s );
 void not_logged_in(Status& s );
 void no_modhash(Status& s );
+void json_error(Status& s, std::string message)
 #endif
