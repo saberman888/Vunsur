a60e6c695b39e62b180272c830a26a2b5f5f5905
saberman888 <saberman8881@gmail.com>
2017-11-10 19:25:14 -0700

    I added an include to Types.hpp

--- include/subreddit/subreddit.hpp
+++ include/subreddit/subreddit.hpp
@@ -9,6 +9,7 @@
 #include "include/condition.hpp"
 #include "include/authdata.hpp"
 #include "include/Error.hpp"
+#include "include/base/Types.hpp"
 
 Status subreddit_about( AccessData* acd, std::string subreddit );
 
