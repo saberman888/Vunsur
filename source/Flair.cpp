b253815f344d61b02242f74fc965849fd679573f
saberman888 <saberman8881@gmail.com>
2017-11-24 03:08:08 -0700

    Implementation of delete_flair

--- source/Flair.cpp
+++ source/Flair.cpp
@@ -95,3 +95,80 @@ Status clear_flair_templates( AccessData* dat, std::string subreddit, FlairType 
 		return s;
 	}
 }
+
+
+Status delete_flair( AccessData* dat, std::string subreddit, std::string flair_template_name )
+{
+	Status s;
+	int state;
+	CURL *handle;
+	CURLcode result;
+	std::string json;
+	
+	handle = curl_easy_init();
+	
+	if( handle )
+	{
+		CURLcode gresult = curl_global_init(CURL_GLOBAL_SSL);
+		if( gresult != CURLE_OK ) {
+			set_curl_global_error(s); return s;
+		} else {
+			struct curl_slist* header = nullptr;
+			std::string authhead = "Authorization: ";
+			authhead += std::string(dat->token_type);
+			authhead += " ";
+			authhead += std::string(dat->token);
+			
+			header = curl_slist_append( header, authhead.c_str() );
+			
+			std::string URL = "https://oauth.reddit.com/r/";
+			URL += subreddit;
+			URL += "/deleteflair";
+			
+			nlohmann::json postdata = {
+				{ "api_type", "json" },
+				{ "name", flair_template_name }
+			};
+			
+			std::string stringjson = postdata.dump();
+			
+			curl_easy_setopt( handle, CURLOPT_URL, URL.c_str() );
+			curl_easy_setopt( handle, CURLOPT_POSTFIELDS, stringjson.c_str() );
+			curl_easy_setopt( handle, CURLOPT_USERAGENT, dat->userAgent.c_str() );
+			curl_easy_setopt( handle, CURLOPT_HTTPHEADER, header );
+			curl_easy_setopt( handle, CURLOPT_SSL_VERIFYPEER, 0L );
+			curl_easy_setopt( handle, CURLOPT_WRITEDATA, &json );
+			curl_easy_setopt( handle, CURLOPT_WRITEFUNCTION, &writedat );
+			
+			#ifdef DEBUG
+			curl_easy_setopt( handle, CURLOPT_VERBOSE, 1L );
+			#endif
+			
+			result = curl_easy_perform(handle);
+			
+			curl_easy_getinfo( handle, CURLINFO_RESPONSE_CODE, &state );
+			s.code = state;
+			
+			curl_easy_cleanup(handle);
+			curl_global_cleanup();
+			
+			if( result != CURLE_OK )
+			{
+				set_curl_strerror(s,result); return s;
+			} else {
+				#ifdef OUTJSOn
+				std::ofstream out("delete_flair.json");
+				out << json;
+				#endif
+				
+				s.cstat = ERROR_NONE;
+				s.message = "";
+				return s;
+			}
+			
+		}
+	} else {
+		set_curl_handle_error(s);
+		return s;
+	}
+}
