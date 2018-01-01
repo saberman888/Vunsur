14a33961f74278e86717ecc40f4a8f58c2d336f3
saberman888 <saberman8881@gmail.com>
2017-12-31 23:54:45 -0700

    Delete condition.cpp

--- source/condition.cpp
+++
@@ -1,7 +0,0 @@
-#include "include/condition.hpp"
-#include <iostream>
-
-void print_status(Status s ) 
-{ 
-	std::cout << "Code: " << s.code << "; " << s.message << "; EStatus: " << s.cstat << std::endl; 
-}
