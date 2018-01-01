3a652a104f754e3409e1a2391a19abe2779c6a18
saberman888 <saberman8881@gmail.com>
2018-01-01 00:05:51 -0700

    Add files via upload

---
+++ include/Vunsur/subreddit/comments/Comment.hpp
@@ -0,0 +1,54 @@
+#ifdef COMMENT_HPP
+#define COMMENT_HPP
+
+#include "base/BaseThing.hpp"
+#include "base/Votable.hpp"
+#include "base/Created.hpp"
+#include <string>
+
+typedef struct r_comment : public BaseThing, public Votable, public Created {
+	
+	bool approved;
+	long approved_at_utc;
+	std::string approved_by;
+	bool archived;
+	std::string author; // can be nulled
+	std::string author_flair_css_class; // can be nulled
+	std::string author_flair_text; // can be nulled
+	long banned_at_utc; // can be nulled
+	std::string banned_by; // can be nulled
+	std::string body;
+	std::string body_html;
+	bool can_guild;
+	bool can_mod_post;
+	bool collapsed;
+	std::string collapsed_reason; // can be nulled
+	int controversiality;
+	int depth;
+	std::string distinguished; // can be nulled
+	bool edited;
+	int gilded;
+	bool ignore_reports;
+	bool is_submitter;
+	std::string link_id;
+	std::vector< std::string > mod_reports;
+	std::string name;
+	int num_reports;
+	std::string parent_id;
+	std::string removal_reason; // can be nulled
+	bool removed;
+	std::vector< Comment* > replies;
+	std::vector< std::string > removal_reason; 
+	bool saved;
+	int score;
+	bool score_hidden;
+	bool spam;
+	bool stickied;
+	std::string subreddit;
+	std::string subreddit_id;
+	std::string subreddit_name_prefixed;
+	std::string restricted_type;
+	std::vector< std::string > user_reports;
+	
+}Comment;
+#endif
---
+++ include/Vunsur/subreddit/comments/Listing.hpp
@@ -0,0 +1,17 @@
+#ifndef LISTING_HPP
+#define LISTING_HPP
+
+#include <string>
+#include <vector>
+
+#include "base/Types.hpp"
+
+template<class T>
+struct Listing {
+	std::string before;
+	std::string after;
+	//std::string modhash;
+	std::vector< T > children;
+};
+
+#endif
