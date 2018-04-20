7463cc6e78e015464dd79009b56034f3ad57b569
saberman888 <saberman8881@gmail.com>
2018-04-20 13:32:10 -0600

    New function: integer_to_string

--- include/Vunsur/vujson.hpp
+++ include/Vunsur/vujson.hpp
@@ -3,6 +3,7 @@
 #include "nlohmann/json.hpp"
 #include <typeinfo>
 #include <string>
+#include <sstream>
 
 bool get_boolean_value(nlohmann::json src);
 long get_long_value(nlohmann::json src);
@@ -10,3 +11,6 @@ int get_integer_value(nlohmann::json src);
 bool does_value_exist(nlohmann::json tag, std::string name);
 std::string get_string_value(nlohmann::json src);
 size_t write_to_string(void *ptr, size_t size, size_t count, void *stream);
+
+// Some other functions that aren't related to Json, but there are too few to belong in their own header
+template<class T> std::string integer_to_string(T val);
