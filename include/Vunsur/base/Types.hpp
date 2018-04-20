3323230b75667d3e35535142c720f52a81dcf5ea
saberman888 <saberman8881@gmail.com>
2018-04-19 22:08:05 -0600

    A new VunType: More = 7

--- include/Vunsur/base/Types.hpp
+++ include/Vunsur/base/Types.hpp
@@ -6,10 +6,11 @@
 typedef enum VunTypes {
 	Comment = 1,
 	Account = 2,
-	Link = 3 ,
-	Message = 4 ,
-	Subreddit = 5 ,
-	Award = 6
+	Link = 3,
+	Message = 4,
+	Subreddit = 5,
+	Award = 6,
+	More = 7
 }VunType;
 
 typedef enum SortTypes {
