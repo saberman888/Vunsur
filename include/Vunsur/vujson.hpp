9aad911db4be961e6270ed6a40f6a7f8917cdc99
saberman888 <saberman8881@gmail.com>
2018-04-19 22:13:29 -0600

    New functions
    New functions include:
    * New functions for extracting Json
    * A better function to write data in curl requests
    * A function to check if a Json tag exists

--- include/Vunsur/vujson.hpp
+++ include/Vunsur/vujson.hpp
@@ -1,14 +1,12 @@
-#ifndef VUJSON_HPP
-#define VUJSON_HPP
+#pragma once
 
-#include "json.hpp"
+#include "nlohmann/json.hpp"
 #include <typeinfo>
 #include <string>
 
 bool get_boolean_value(nlohmann::json src);
 long get_long_value(nlohmann::json src);
 int get_integer_value(nlohmann::json src);
+bool does_value_exist(nlohmann::json tag, std::string name);
 std::string get_string_value(nlohmann::json src);
-
-#endif
-
+size_t write_to_string(void *ptr, size_t size, size_t count, void *stream);
